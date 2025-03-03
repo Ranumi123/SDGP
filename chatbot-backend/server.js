6b13db5361ea203e0cfeaef5307eeab9ba48eb1a
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-03-03 18:15:52 +0530

    update the code

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -37,7 +37,6 @@
 // console.log(result.response.text());
 
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
