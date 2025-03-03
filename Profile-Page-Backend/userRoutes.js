ae5922eb007a1a141fa504340603405d8a7cdc54
Diwanga Munasinghe <diwangapawani405@gmail.com>
2025-03-03 10:47:14 +0530

    backend codes of userController and userRoutes

---
+++ Profile-Page-Backend/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getUserProfile, updatePreferences, loginUser, registerUser } = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', authMiddleware, getUserProfile);
+router.put('/preferences', authMiddleware, updatePreferences);
+
+module.exports = router;
