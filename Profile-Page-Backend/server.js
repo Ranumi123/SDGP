de9f0f1470d5db629ff15956c844dbbbb6077f35
Diwanga Munasinghe <diwangapawani405@gmail.com>
2025-03-04 07:44:56 +0530

    Commented on Server Code

--- Profile-Page-Backend/server.js
+++ Profile-Page-Backend/server.js
@@ -1,15 +1,20 @@
+// Import required modules
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
+// Load environment variables from .env file into process.env
 dotenv.config();
 const app = express();
 
+// Initialize the Express application
 app.use(cors());
 app.use(express.json());
 
+// Enable CORS to allow requests from different origins (useful for API communication)
 app.use('/api/users', userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
